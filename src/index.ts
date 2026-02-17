ae9222464d3ac253d1a32e7d7f3264a89735fae5
Thelegendofkira <mandadijaideep@gmail.com>
2026-02-17 18:28:35 +0530

    removed cors specifications

--- src/index.ts
+++ src/index.ts
@@ -12,9 +12,7 @@ import cors from "cors";
 
 const app = express();
 app.use(express.json());
-app.use(cors({
-    origin: "http://localhost:5173"
-}));
+app.use(cors());
 app.post('/api/signup', async (req, res) => {
     let response;
     try {
